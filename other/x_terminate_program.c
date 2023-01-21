186c77d4e2a55fa15cfa4ff936bc1eac712c0d66
lightsidecode <lightsidecode@gmail.com>
2023-01-22 00:19:39 +0100

    kata-x_terminate_program implementacja

---
+++ other/x_terminate_program.c
@@ -0,0 +1,29 @@
+/*
+    Zabawa, ktora polega na wprowadzaniu znakow z klawiatury. Jezeli wprowadzimy 'x' to
+    program zostanie zatrzymany.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_terminate_program(char sign);
+
+int main(void) {
+
+    char sign = ' ';
+
+    printf("Rozpocznij wprowadzanie znakow... \n");
+    do {
+        sign = getc(stdin);
+        putc(sign, stdout);
+    } while(!is_terminate_program(sign));
+
+    printf("\n");
+    printf("Koniec zabawy... \n");
+
+    return 0;
+}
+
+bool is_terminate_program(char sign) {
+    return sign == 'x';
+}
