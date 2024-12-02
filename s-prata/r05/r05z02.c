f2406ac1543c9e19ed7458c60ede3afd5d18ffc7
Tomasz Gadek <gadektomek@gmail.com>
2024-12-02 23:11:22 +0100

    prata, wyswietl liczby calkowite

---
+++ s-prata/r05/r05z02.c
@@ -0,0 +1,28 @@
+/*
+    r05z02.c: Wyświetl liczby całkowite
+
+    2. Napisz program, który prosi o podanie liczby całkowitej, a następnie 
+    wyświetla wszystkie liczby całkowite od tej wartości do wartości większej o 10 (włącznie). 
+    (Jeśli zatem wpisano liczbę 5, program wyświetla liczby od 5 do 15.)
+*/
+
+const int SHIFT = 10;
+
+#include <stdio.h>
+
+int main(void) {
+
+    int digit_start = 0, digit_stop = 0;
+
+    printf("Podaj liczbe poczatkowa: ");
+    scanf("%d", &digit_start);
+
+    digit_stop = digit_start + SHIFT;
+
+    while(digit_start <= digit_stop) {
+        printf("%d ", digit_start);
+        digit_start += 1;
+    }
+
+    putchar('\n');
+}
