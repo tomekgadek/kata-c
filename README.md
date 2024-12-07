c3ca5447fc11376ac2b1c99f8bb0118f9acc75f6
Tomasz Gadek <gadektomek@gmail.com>
2024-12-07 20:37:16 +0100

    prata, wspak

--- README.md
+++ README.md
@@ -279,4 +279,5 @@ Język C, Szkoła programowania, Stephen Prata - rozwiązania zadań
     + [r06z02.c: Zbuduj trójkąt z dolarów](./s-prata/r06/r06z02.c),
     + [r06z03.c: Zbuduj trójkąt zawierający znaki F..A](./s-prata/r06/r06z03.c),
     + [r06z04.c: Zbuduj piramidę znakową](./s-prata/r06/r06z04.c),
-    + [r06z05.c: Wyświetlanie tabeli](./s-prata/r06/r06z05.c).
+    + [r06z05.c: Wyświetlanie tabeli](./s-prata/r06/r06z05.c),
+    + [r06z06.c: Wspak](./s-prata/r06/r06z06.c).
---
+++ s-prata/r06/r06z06.c
@@ -0,0 +1,35 @@
+/*
+    r06z06.c: Wspak
+
+    6. Napisz program, który wczytuje pojedyncze słowo do tablicy 
+    znakowej, a następnie wyświetla je wspak.
+    Wskazówka: Użyj funkcji 'strlen()' do obliczenia indeksu ostatniego 
+    znaku w tablicy (patrz Rozdzial 4 "Łańcuch znakowe i formatowane
+    wejście / wyjście").
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define STR_SIZE 20 + 1
+
+int main(void) {
+
+    char message[STR_SIZE];
+
+    printf("Podaj slowo: ");
+    scanf("%s", message);
+
+    int last_idx = strlen(message) - 1;
+
+    printf("size = %d \n", last_idx);
+
+    for(int i = last_idx; i >= 0; --i) {
+
+        putchar(message[i]);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
