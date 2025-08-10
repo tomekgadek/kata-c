ea07d4bd430b89a789a759550fb7b9750042dae0
tomekgadek <gadektomek@gmail.com>
2025-08-10 14:30:15 +0200

    prata, numeracja slow (temat zadania)

---
+++ s-prata/r12/r12z07.c
@@ -0,0 +1,8 @@
+/*
+    r12z07.c: Numeracja słów
+
+    7. Zmodyfikuj program z listingu 12.3 tak, aby słowa były ponumerowane 
+    według kolejności dodawania ich do listy, poczynając od numeru 1.
+    Dopilnuj, aby przy drugim uruchomieniu programu numeracja rozpoczynała
+    się od miejsca, w którym została przerwana.
+*/
