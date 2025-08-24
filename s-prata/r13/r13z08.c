4998350fdf8cdeccef64caaa7bc81565ed33713c
tomekgadek <gadektomek@gmail.com>
2025-08-24 22:05:33 +0200

    prata, przygotowanie tresci zadania r13z08

---
+++ s-prata/r13/r13z08.c
@@ -0,0 +1,11 @@
+/*
+    r13z08.c: Sortowanie i zapisywanie słów do pliku
+
+    8. Napisz program interaktywny, który pozwala wpisać do 20 słów, wyświetla 
+    je w kolejności alfabetycznej (zajrzyj do programu sortującego łańcuchy z 
+    Rozdziału 11 „Łańcuchy znakowe i funkcje łańcuchowe”), a następnie pyta 
+    użytkownika, czy powinny one zostać zapisane w pliku. Jeśli użytkownik 
+    odpowie twierdząco, program powinien poprosić o podanie nazwy pliku, a 
+    następnie dokonać zapisu.
+*/
+
