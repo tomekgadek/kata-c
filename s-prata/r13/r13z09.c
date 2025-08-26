e97c459f32cbef01456bdf54f9b2b1e8efa01b9c
tomekgadek <gadektomek@gmail.com>
2025-08-26 22:25:19 +0200

    prata, przygotowanie tresci zadania r13z09

---
+++ s-prata/r13/r13z09.c
@@ -0,0 +1,16 @@
+/*
+    r13z09.c: Pobieranie najbliższej liczby całkowitej z wejścia
+
+    9. Napisz funkcję, która pobiera i porzuca dane wejściowe aż do momentu 
+    odczytania cyfry, następnie zapisuje tę cyfrę i wszystkie kolejne aż do 
+    napotkania znaku nie będącego cyfrą. Znak nienumeryczny zostaje umieszczony 
+    z powrotem w strumieniu wejściowym, a funkcja przetwarza ciąg cyfr na 
+    wartość liczbową. Wartość ta powinna zostać dostarczona funkcji wywołującej 
+    za pośrednictwem argumentu wskaźnikowego. Funkcja powinna zwracać EOF w 
+    przypadku napotkania końca pliku lub 1 w pozostałych przypadkach. 
+    Skorzystaj z funkcji getc() i ungetc(). Mówiąc w skrócie, funkcja powinna 
+    znaleźć najbliższą liczbę całkowitą w danych wejściowych, niezależnie od 
+    tego, czy występuje ona samodzielnie, czy jest otoczona tekstem 
+    (np. w7niebie).
+*/
+
