87d53c4200c1a394f4fbaecf6ea63fdcb97c8ed3
tomekgadek <gadektomek@gmail.com>
2025-09-05 06:22:51 +0200

    prata, linie lotnicze colossus - program do rezerwacji miejsc (tresc zadania)

---
+++ s-prata/r14/r14z08.c
@@ -0,0 +1,28 @@
+/*
+    r14z08.c: Linie lotnicze Colossus - program do rezerwacji miejsc
+
+    8. Flota linii lotniczych Colossus składa się z jednego samolotu o 12 
+    miejscach. Samolot ten odbywa jeden rejs dziennie. Napisz program do 
+    rezerwacji miejsc spełniający następujące warunki:
+    
+    a. Program wykorzystuje tablicę 12 struktur. Każda z nich powinna 
+       przechowywać numer identyfikacyjny miejsca, znacznik określający, czy 
+       miejsce jest wolne, oraz imię i nazwisko osoby, która dokonała rezerwacji.
+    b. Program wyświetla poniższe menu:
+    
+          Aby wybrać opcję, wpisz jej oznaczenie literowe:
+       
+          a. Pokaż liczbę pustych miejsc
+          b. Pokaż listę pustych miejsc
+          c. Pokaż alfabetyczną listę miejsc
+          d. Zarezerwuj miejsce dla klienta
+          e. Usuń rezerwację miejsca
+          f. Koniec
+
+    c. Program wykonuje czynności zapowiedziane w menu. Opcje d) i e) wymagają 
+       podania dodatkowych danych; każda z nich powinna umożliwić przerwanie 
+       wpisywania.
+    d. Po wykonaniu jednej z funkcji program powinien ponownie wyświetlić menu 
+       (nie dotyczy opcji f)).
+    e. Informacje o rezerwacjach są przechowywane w pliku.
+*/
