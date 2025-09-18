e8a13c38ef234a982046c410f234cda54861de1a
tomekgadek <gadektomek@gmail.com>
2025-09-18 20:27:24 +0200

    prata, plik naglowkowy zawierajacy definicje makr

---
+++ s-prata/r16/r16z01.h
@@ -0,0 +1,14 @@
+/*
+    r16z01.h: Plik nagłówkowy zawierający definicje makr
+
+    1. Zacznij opracowywać plik nagłówkowy zawierający definicje makr, które
+    będziesz wykorzystywał w swoich programach.
+*/
+
+#ifndef CONST_H
+#define CONST_H
+
+#define true 1
+#define false 0
+
+#endif // CONST_H
