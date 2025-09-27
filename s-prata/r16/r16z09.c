7dd571410fd857d65578a64bd940da1ee30928dc
tomekgadek <gadektomek@gmail.com>
2025-09-27 23:25:46 +0200

    prata, oirzadkowanie tablicy struktur (tresc zadania)

---
+++ s-prata/r16/r16z09.c
@@ -0,0 +1,7 @@
+/*
+    r16z09.c: Porządkowanie tablicy struktur
+
+    9. Zmodyfikuj listing 16.13 tak, aby porządkował tablicę typu struct osoba 
+    (zdefiniowaną w tekście po listingu) zamiast tablicy typu double. Użyj 
+    mniejszej liczby elementów i zainicjalizuj je własnoręcznie.
+*/
